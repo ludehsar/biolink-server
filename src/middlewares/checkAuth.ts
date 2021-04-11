2d9f6089827e2e903370fee57130656335bbeb0c
Rashedul Alam <rashedulalam112112@gmail.com>
2021-04-11 18:14:44 +0600

    Implement invalidate cookies for unsuccessful verification

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -1,4 +1,4 @@
-import { Response, NextFunction } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { verify } from 'jsonwebtoken'
 
 import { accessTokenSecret, refreshTokenSecret } from '../config/app.config'
@@ -6,47 +6,64 @@ import { refreshTokenCookieOptions, accessTokenCookieOptions } from '../config/c
 import { User } from '../models/entities/User'
 import { createAuthTokens } from '../utils/createAuthTokens'
 
-const authCheck = async (req: any, res: Response, next: NextFunction) => {
+const invalidateToken = (res: Response) => {
+  res.cookie('refresh_token', '', refreshTokenCookieOptions)
+  res.cookie('access_token', '', accessTokenCookieOptions)
+}
+
+const generateNewToken = (user: User, res: Response) => {
+  const { refreshToken: newRefreshToken, accessToken: newAccessToken } = createAuthTokens(user)
+
+  res.cookie('refresh_token', newRefreshToken, refreshTokenCookieOptions)
+  res.cookie('access_token', newAccessToken, accessTokenCookieOptions)
+}
+
+const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
   const refreshToken = req.cookies.refresh_token
   const accessToken = req.cookies.access_token
+
+  // If no token, return
   if (!refreshToken && !accessToken) {
+    invalidateToken(res)
     return next()
   }
 
+  // Verify access token
   try {
     const data = verify(accessToken, accessTokenSecret) as any
 
     // For token validation
     const user = await User.findOne(data.userId)
 
-    if (user) req.userId = user.id
+    // If token tempared
+    if (!user) {
+      invalidateToken(res)
+    }
+
     return next()
   } catch {}
 
   if (!refreshToken) {
+    invalidateToken(res)
     return next()
   }
 
+  // Verify refresh token
   try {
     const data = verify(refreshToken, refreshTokenSecret) as any
     const user = await User.findOne(data.userId)
 
-    if (!user || user.totalLogin !== data.count) {
+    if (!user) {
+      invalidateToken(res)
       return next()
     }
 
-    user.totalLogin++
-
     await User.save(user)
-    req.userId = data.userId
 
-    const { refreshToken: newRefreshToken, accessToken: newAccessToken } = createAuthTokens(user)
-
-    res.cookie('refresh_token', newRefreshToken, refreshTokenCookieOptions)
-    res.cookie('access_token', newAccessToken, accessTokenCookieOptions)
+    generateNewToken(user, res)
   } catch {}
 
   next()
 }
 
-export default authCheck
+export default checkAuth
--- src/utils/createAuthTokens.ts
+++ src/utils/createAuthTokens.ts
@@ -4,7 +4,7 @@ import { refreshTokenSecret, accessTokenSecret } from '../config/app.config'
 import { User } from '../models/entities/User'
 
 export const createAuthTokens = (user: User) => {
-  const refreshToken = sign({ userId: user.id, count: user.totalLogin }, refreshTokenSecret, { expiresIn: '7d' })
+  const refreshToken = sign({ userId: user.id }, refreshTokenSecret, { expiresIn: '7d' })
   const accessToken = sign({ userId: user.id }, accessTokenSecret, { expiresIn: '5min' })
 
   return { refreshToken, accessToken }
