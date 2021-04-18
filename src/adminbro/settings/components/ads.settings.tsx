3e7bc1fb24b7115eadc64a82fdb912dfabb68a79
Rashedul Alam <rashedulalam112112@gmail.com>
2021-04-18 21:12:28 +0600

    Implemented sotring ads settings to database

--- src/adminbro/settings/components/ads.settings.tsx
+++ src/adminbro/settings/components/ads.settings.tsx
@@ -1,4 +1,5 @@
 import { FormGroup } from '@admin-bro/design-system'
+import { Formik, Form } from 'formik'
 import { withNotice } from 'admin-bro'
 import React from 'react'
 
@@ -12,37 +13,95 @@ import {
   SettingsTabContainer,
 } from './CommonSettings.styled'
 import { CommonSettingsProps } from './CommonSettingsProps'
+import { saveAdsSettings } from '../actions/adsSettingsAction'
+import { AdsSystemSettings } from '../../../models/jsonTypes/AdsSystemSettings'
 
-const AdsSettings: React.FC<CommonSettingsProps> = (props) => {
-  const { className, id } = props
+const AdsSettings: React.FC<CommonSettingsProps> = ({ addNotice, ...props }) => {
+  const { className, id, value } = props
+
+  const handleSubmit = async (values: AdsSystemSettings): Promise<void> => {
+    await saveAdsSettings(values).catch(() => {
+      addNotice({
+        message: 'Unable to save data to the database.',
+        type: 'error',
+      })
+      return
+    })
+    addNotice({
+      message: 'Successfully updated main settings',
+      type: 'success',
+    })
+  }
+
   return (
     <SettingsTabContainer {...{ className, id }}>
       <Title>
         <FormHelper>These fields accept text or html.</FormHelper>
       </Title>
-      <FormGroup>
-        <FormLabel>Header</FormLabel>
-        <FormTextArea />
-      </FormGroup>
+      <Formik
+        enableReinitialize={true}
+        initialValues={{
+          header: (value as AdsSystemSettings)?.header || '',
+          footer: (value as AdsSystemSettings)?.footer || '',
+          biolinkPageHeader: (value as AdsSystemSettings)?.biolinkPageHeader || '',
+          biolinkPageFooter: (value as AdsSystemSettings)?.biolinkPageFooter || '',
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          handleSubmit(values as AdsSystemSettings)
+          setSubmitting(false)
+          return
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+          <Form onSubmit={handleSubmit} method="post">
+            <FormGroup>
+              <FormLabel>Header</FormLabel>
+              <FormTextArea
+                name="header"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.header}
+              />
+            </FormGroup>
 
-      <FormGroup>
-        <FormLabel>Footer</FormLabel>
-        <FormTextArea />
-      </FormGroup>
+            <FormGroup>
+              <FormLabel>Footer</FormLabel>
+              <FormTextArea
+                name="footer"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.footer}
+              />
+            </FormGroup>
 
-      <FormGroup>
-        <FormLabel>Biolink Page Header</FormLabel>
-        <FormTextArea />
-      </FormGroup>
+            <FormGroup>
+              <FormLabel>Biolink Page Header</FormLabel>
+              <FormTextArea
+                name="biolinkPageHeader"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.biolinkPageHeader}
+              />
+            </FormGroup>
 
-      <FormGroup>
-        <FormLabel>Biolink Page Footer</FormLabel>
-        <FormTextArea />
-      </FormGroup>
+            <FormGroup>
+              <FormLabel>Biolink Page Footer</FormLabel>
+              <FormTextArea
+                name="biolinkPageFooter"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.biolinkPageFooter}
+              />
+            </FormGroup>
 
-      <FormCTAGroup>
-        <FormSubmitButton>Update</FormSubmitButton>
-      </FormCTAGroup>
+            <FormCTAGroup>
+              <FormSubmitButton type="submit" disabled={isSubmitting}>
+                Update
+              </FormSubmitButton>
+            </FormCTAGroup>
+          </Form>
+        )}
+      </Formik>
     </SettingsTabContainer>
   )
 }
