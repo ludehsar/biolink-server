a54e354df7e6bc850fbf85c4c27b340ef27e12af
Rashedul Alam <rashedulalam112112@gmail.com>
2021-04-19 08:15:39 +0600

    Configured facebook login settings

--- src/adminbro/settings/components/facebook.settings.tsx
+++ src/adminbro/settings/components/facebook.settings.tsx
@@ -1,5 +1,6 @@
 import { FormGroup } from '@admin-bro/design-system'
 import { withNotice } from 'admin-bro'
+import { Formik, Form } from 'formik'
 import React from 'react'
 
 import {
@@ -10,26 +11,67 @@ import {
   SettingsTabContainer,
 } from './CommonSettings.styled'
 import { CommonSettingsProps } from './CommonSettingsProps'
+import { FacebookSystemSettings } from '../../../models/jsonTypes/FacebookSystemSettings'
+import { saveFacebookLoginSettings } from '../actions/facebookSettingsAction'
 
 const FacebookSettings: React.FC<CommonSettingsProps> = (props) => {
-  const { className, id } = props
+  const { className, id, value, addNotice } = props
+
+  const handleSubmit = async (values: FacebookSystemSettings): Promise<void> => {
+    await saveFacebookLoginSettings(values).catch(() => {
+      addNotice({
+        message: 'Unable to save data to the database.',
+        type: 'error',
+      })
+      return
+    })
+    addNotice({
+      message: 'Successfully updated settings',
+      type: 'success',
+    })
+  }
+
   return (
     <SettingsTabContainer {...{ className, id }}>
-      <FormGroup>
-        <FormLabel>Enable Facebook Login</FormLabel>
-        <FormInput as="select">
-          <FormInput as="option" value={1}>
-            Yes
-          </FormInput>
-          <FormInput as="option" value={0}>
-            No
-          </FormInput>
-        </FormInput>
-      </FormGroup>
+      <Formik
+        enableReinitialize={true}
+        initialValues={{
+          enableFacebookLogin: (value as FacebookSystemSettings)?.enableFacebookLogin || 'no',
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          handleSubmit(values as FacebookSystemSettings)
+          setSubmitting(false)
+          return
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+          <Form onSubmit={handleSubmit} method="post">
+            <FormGroup>
+              <FormLabel>Enable Facebook Login</FormLabel>
+              <FormInput
+                as="select"
+                name="enableFacebookLogin"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.enableFacebookLogin}
+              >
+                <FormInput as="option" value={'yes'}>
+                  Yes
+                </FormInput>
+                <FormInput as="option" value={'no'}>
+                  No
+                </FormInput>
+              </FormInput>
+            </FormGroup>
 
-      <FormCTAGroup>
-        <FormSubmitButton>Update</FormSubmitButton>
-      </FormCTAGroup>
+            <FormCTAGroup>
+              <FormSubmitButton type="submit" disabled={isSubmitting}>
+                Update
+              </FormSubmitButton>
+            </FormCTAGroup>
+          </Form>
+        )}
+      </Formik>
     </SettingsTabContainer>
   )
 }
