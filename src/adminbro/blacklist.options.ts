e197fe91de4226fe6221b777d4c86e3205f73c5a
Rashedul Alam <rashedulalam112112@gmail.com>
2021-05-10 15:26:32 +0600

    Enabled blacklist access according to the admin roles

--- src/adminbro/blacklist.options.ts
+++ src/adminbro/blacklist.options.ts
@@ -15,12 +15,69 @@ export const blacklistOptions: ResourceOptions = {
   actions: {
     new: {
       showInDrawer: true,
+      isAccessible: ({ currentAdmin }): boolean => {
+        if (!currentAdmin) return false
+        return (
+          currentAdmin.role.roleName === 'Administrator' ||
+          currentAdmin.role['roleSettings.1.canCreate'] === 'true'
+        )
+      },
     },
-    show: {
-      isVisible: false,
+    bulkDelete: {
+      isAccessible: ({ currentAdmin }): boolean => {
+        if (!currentAdmin) return false
+        return (
+          currentAdmin.role.roleName === 'Administrator' ||
+          currentAdmin.role['roleSettings.1.canDelete'] === 'true'
+        )
+      },
+    },
+    delete: {
+      isAccessible: ({ currentAdmin }): boolean => {
+        if (!currentAdmin) return false
+        return (
+          currentAdmin.role.roleName === 'Administrator' ||
+          currentAdmin.role['roleSettings.1.canDelete'] === 'true'
+        )
+      },
     },
     edit: {
       showInDrawer: true,
+      isAccessible: ({ currentAdmin }): boolean => {
+        if (!currentAdmin) return false
+        return (
+          currentAdmin.role.roleName === 'Administrator' ||
+          currentAdmin.role['roleSettings.1.canEdit'] === 'true'
+        )
+      },
+    },
+    list: {
+      isAccessible: ({ currentAdmin }): boolean => {
+        if (!currentAdmin) return false
+        return (
+          currentAdmin.role.roleName === 'Administrator' ||
+          currentAdmin.role['roleSettings.1.canShowList'] === 'true'
+        )
+      },
+    },
+    search: {
+      isAccessible: ({ currentAdmin }): boolean => {
+        if (!currentAdmin) return false
+        return (
+          currentAdmin.role.roleName === 'Administrator' ||
+          currentAdmin.role['roleSettings.1.canShowList'] === 'true'
+        )
+      },
+    },
+    show: {
+      isVisible: false,
+      isAccessible: ({ currentAdmin }): boolean => {
+        if (!currentAdmin) return false
+        return (
+          currentAdmin.role.roleName === 'Administrator' ||
+          currentAdmin.role['roleSettings.1.canShow'] === 'true'
+        )
+      },
     },
   },
   navigation: {
