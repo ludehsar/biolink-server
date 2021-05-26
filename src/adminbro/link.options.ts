9656941034203a7acdb6525c6c5b3fb9d25925df
Rashedul Alam <rashedulalam112112@gmail.com>
2021-05-26 12:35:00 +0600

    Added shor links options to admin panel

--- src/adminbro/link.options.ts
+++ src/adminbro/link.options.ts
@@ -3,8 +3,25 @@ import { ResourceOptions } from 'admin-bro'
 import { Link } from '../models/entities/Link'
 
 export const linkOptions: ResourceOptions = {
-  listProperties: ['userId', 'biolinkId', 'url', 'clicks', 'status', 'createdAt', 'updatedAt'],
-  filterProperties: ['userId', 'biolinkId', 'url', 'status', 'createdAt', 'updatedAt'],
+  listProperties: [
+    'userId',
+    'biolinkId',
+    'url',
+    'shortenedUrl',
+    'clicks',
+    'status',
+    'createdAt',
+    'updatedAt',
+  ],
+  filterProperties: [
+    'userId',
+    'biolinkId',
+    'url',
+    'shortenedUrl',
+    'status',
+    'createdAt',
+    'updatedAt',
+  ],
   actions: {
     new: {
       isVisible: false,
