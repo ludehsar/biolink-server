0b47cdb4031cc14c9e3437b7c4883dd669d6cf84
Rashedul Alam <rashedulalam112112@gmail.com>
2021-06-23 16:16:20 +0600

    Implemented black lists in admin dashboard

--- src/resolvers/admin/blackList.admin.resolver.ts
+++ src/resolvers/admin/blackList.admin.resolver.ts
@@ -1,12 +1,32 @@
 import { Arg, Query, Resolver } from 'type-graphql'
 import { ConnectionArgs } from '../../input-types'
 import { BlackListConnection } from '../../object-types'
-import { getBlackListedUsernamesPaginated } from '../../services'
+import {
+  getBlackListedBadWordsPaginated,
+  getBlackListedEmailsPaginated,
+  getBlackListedUsernamesPaginated,
+} from '../../services'
 import { User } from '../../entities'
 import { CurrentAdmin } from '../../decorators'
 
 @Resolver()
 export class BlackListAdminResolver {
+  @Query(() => BlackListConnection, { nullable: true })
+  async getAllBlackListedBadWords(
+    @Arg('options') options: ConnectionArgs,
+    @CurrentAdmin() adminUser: User
+  ): Promise<BlackListConnection> {
+    return await getBlackListedBadWordsPaginated(options, adminUser)
+  }
+
+  @Query(() => BlackListConnection, { nullable: true })
+  async getAllBlackListedEmails(
+    @Arg('options') options: ConnectionArgs,
+    @CurrentAdmin() adminUser: User
+  ): Promise<BlackListConnection> {
+    return await getBlackListedEmailsPaginated(options, adminUser)
+  }
+
   @Query(() => BlackListConnection, { nullable: true })
   async getAllBlackListedUsernames(
     @Arg('options') options: ConnectionArgs,
