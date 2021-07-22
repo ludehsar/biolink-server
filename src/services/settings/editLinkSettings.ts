94e471118946e863a12617e82bc4ca00e4dfd4fd
Rashedul Alam <rashedulalam112112@gmail.com>
2021-07-22 21:19:56 +0600

    Implemented editing link settings

---
+++ src/services/settings/editLinkSettings.ts
@@ -0,0 +1,58 @@
+import { LinkSettingsResponse } from '../../object-types'
+import { Settings, User } from '../../entities'
+import { ErrorCode, MyContext } from '../../types'
+import { captureUserActivity } from '../../services'
+import { LinkSettingsInput } from '../../input-types'
+import { LinkSystemSettings } from 'json-types'
+
+export const editLinkSettings = async (
+  options: LinkSettingsInput,
+  adminUser: User,
+  context: MyContext
+): Promise<LinkSettingsResponse> => {
+  if (!adminUser) {
+    return {
+      errors: [
+        {
+          errorCode: ErrorCode.USER_NOT_AUTHENTICATED,
+          message: 'User is not authenticated',
+        },
+      ],
+    }
+  }
+
+  const adminRole = await adminUser.adminRole
+
+  if (!adminRole || adminRole.roleName !== 'Administrator') {
+    return {
+      errors: [
+        {
+          errorCode: ErrorCode.USER_NOT_AUTHORIZED,
+          message: 'User is not authorized',
+        },
+      ],
+    }
+  }
+
+  let link = await Settings.findOne({ where: { key: 'link' } })
+
+  if (!link) {
+    link = await Settings.create({
+      key: 'link',
+    }).save()
+  }
+
+  const linkSettings = link.value as LinkSystemSettings
+
+  linkSettings.branding = options.branding || ''
+  linkSettings.enableGoogleSafeBrowsing = options.enableGoogleSafeBrowsing || false
+  linkSettings.enableLinkShortenerSystem = options.enableLinkShortenerSystem || false
+  linkSettings.enablePhishtank = options.enablePhishtank || false
+
+  link.value = linkSettings
+  await link.save()
+
+  await captureUserActivity(adminUser, context, `Changed link settings`, false)
+
+  return { settings: linkSettings }
+}
