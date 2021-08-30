bb4213a4e41181ca155aab9dda74cd58018a6ba7
Rashedul Alam <rashedulalam112112@gmail.com>
2021-08-30 17:28:01 +0600

    Implemented getting sent email addresses for referrals in pagination result

--- src/resolvers/app/referral.app.resolver.ts
+++ src/resolvers/app/referral.app.resolver.ts
@@ -1,24 +1,27 @@
 import { CurrentUser } from '../../decorators'
 import { User } from '../../entities'
-import { ReferralInput } from '../../input-types'
-import { ReferralResponse } from '../../object-types'
-import { getUserReferrals, createReferrals } from '../../services'
+import { ConnectionArgs, ReferralInput } from '../../input-types'
+import { DefaultResponse, ReferralConnection } from '../../object-types'
+import { createReferrals, getSentEmailReferralsPaginated } from '../../services'
 import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 import { MyContext } from '../../types'
 
 @Resolver()
 export class ReferralResolver {
-  @Query(() => ReferralResponse)
-  async getReferralsList(@CurrentUser() user: User): Promise<ReferralResponse> {
-    return await getUserReferrals(user)
+  @Query(() => ReferralConnection)
+  async getSentEmailReferrals(
+    @Arg('options') options: ConnectionArgs,
+    @CurrentUser() user: User
+  ): Promise<ReferralConnection> {
+    return await getSentEmailReferralsPaginated(options, user)
   }
 
-  @Mutation(() => ReferralResponse)
+  @Mutation(() => DefaultResponse)
   async createReferrals(
     @Arg('referralOptions') referralOptions: ReferralInput,
     @Ctx() context: MyContext,
     @CurrentUser() user: User
-  ): Promise<ReferralResponse> {
+  ): Promise<DefaultResponse> {
     return await createReferrals(referralOptions, user, context)
   }
 }
---
+++ src/services/referral/getSentEmailReferralsPaginated.ts
@@ -0,0 +1,144 @@
+import { ConnectionArgs } from 'input-types'
+import moment from 'moment'
+import { Brackets, getRepository } from 'typeorm'
+import { Referral, User } from '../../entities'
+import { ReferralConnection } from '../../object-types'
+import { ErrorCode } from '../../types'
+
+export const getSentEmailReferralsPaginated = async (
+  options: ConnectionArgs,
+  user: User
+): Promise<ReferralConnection> => {
+  if (!user) {
+    return {
+      errors: [
+        {
+          errorCode: ErrorCode.USER_NOT_AUTHENTICATED,
+          message: 'User not authenticated',
+        },
+      ],
+    }
+  }
+
+  // Getting before and after cursors from connection args
+  let before = null
+  if (options.before) before = Buffer.from(options.before, 'base64').toString()
+
+  let after = null
+  if (options.after)
+    after = moment(Buffer.from(options.after, 'base64').toString())
+      .add(1, 'second')
+      .format('YYYY-MM-DD HH:mm:ss')
+
+  // Gettings the directories and preparing objects
+  const connection = new ReferralConnection()
+
+  const qb = getRepository(Referral)
+    .createQueryBuilder('referral')
+    .where(`referral.referredById = :userId`, { userId: user.id })
+    .andWhere(
+      new Brackets((qb) => {
+        qb.where(`LOWER(referral.referredByEmail) like :query`, {
+          query: `%${options.query.toLowerCase()}%`,
+        })
+          .orWhere(`LOWER(referral.referredByName) like :query`, {
+            query: `%${options.query.toLowerCase()}%`,
+          })
+          .orWhere(`LOWER(referral.referredToEmail) like :query`, {
+            query: `%${options.query.toLowerCase()}%`,
+          })
+          .orWhere(`LOWER(referral.referredToName) like :query`, {
+            query: `%${options.query.toLowerCase()}%`,
+          })
+      })
+    )
+
+  if (before) {
+    qb.andWhere('referral.createdAt < :before', { before })
+      .orderBy('referral.createdAt', 'DESC')
+      .limit(options.first)
+  } else if (after) {
+    qb.andWhere('referral.createdAt > :after', { after })
+      .orderBy('referral.createdAt', 'ASC')
+      .limit(options.first)
+  } else {
+    qb.orderBy('referral.createdAt', 'ASC').limit(options.first)
+  }
+
+  const referrals = await qb.getMany()
+
+  if (before) {
+    referrals.reverse()
+  }
+
+  const firstReferral = referrals[0]
+  const lastReferral = referrals[referrals.length - 1]
+
+  // Checking if previous page and next page is present
+  const minDate = moment(firstReferral?.createdAt).format('YYYY-MM-DD HH:mm:ss')
+  const maxDate = moment(lastReferral?.createdAt).add(1, 's').format('YYYY-MM-DD HH:mm:ss') // add changes the dates, so it should be at the last
+
+  connection.edges = referrals.map((referral) => ({
+    node: referral,
+    cursor: Buffer.from(moment(referral.createdAt).format('YYYY-MM-DD HH:mm:ss')).toString(
+      'base64'
+    ),
+  }))
+
+  const previousReferrals = await getRepository(Referral)
+    .createQueryBuilder('referral')
+    .where(`referral.referredById = :userId`, { userId: user.id })
+    .andWhere(
+      new Brackets((qb) => {
+        qb.where(`LOWER(referral.referredByEmail) like :query`, {
+          query: `%${options.query.toLowerCase()}%`,
+        })
+          .orWhere(`LOWER(referral.referredByName) like :query`, {
+            query: `%${options.query.toLowerCase()}%`,
+          })
+          .orWhere(`LOWER(referral.referredToEmail) like :query`, {
+            query: `%${options.query.toLowerCase()}%`,
+          })
+          .orWhere(`LOWER(referral.referredToName) like :query`, {
+            query: `%${options.query.toLowerCase()}%`,
+          })
+      })
+    )
+    .andWhere('referral.createdAt < :minDate', { minDate })
+    .getMany()
+
+  const nextReferrals = await getRepository(Referral)
+    .createQueryBuilder('referral')
+    .where(`referral.referredById = :userId`, { userId: user.id })
+    .andWhere(
+      new Brackets((qb) => {
+        qb.where(`LOWER(referral.referredByEmail) like :query`, {
+          query: `%${options.query.toLowerCase()}%`,
+        })
+          .orWhere(`LOWER(referral.referredByName) like :query`, {
+            query: `%${options.query.toLowerCase()}%`,
+          })
+          .orWhere(`LOWER(referral.referredToEmail) like :query`, {
+            query: `%${options.query.toLowerCase()}%`,
+          })
+          .orWhere(`LOWER(referral.referredToName) like :query`, {
+            query: `%${options.query.toLowerCase()}%`,
+          })
+      })
+    )
+    .andWhere('referral.createdAt > :maxDate', { maxDate })
+    .getMany()
+
+  connection.pageInfo = {
+    startCursor: Buffer.from(
+      moment(firstReferral?.createdAt).format('YYYY-MM-DD HH:mm:ss') || ''
+    ).toString('base64'),
+    endCursor: Buffer.from(
+      moment(lastReferral?.createdAt).format('YYYY-MM-DD HH:mm:ss') || ''
+    ).toString('base64'),
+    hasNextPage: !!nextReferrals.length,
+    hasPreviousPage: !!previousReferrals.length,
+  }
+
+  return connection
+}
