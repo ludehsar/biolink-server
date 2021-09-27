278ff8d15188d2c59df457b717493aa4cabb41d9
Rashedul Alam <rashedulalam112112@gmail.com>
2021-09-28 00:27:30 +0600

    Fixed migration issue

--- src/migrations/1632766796606-AddedLinkMinimalIconsLocation.ts
+++ src/migrations/1632766796606-AddedLinkMinimalIconsLocation.ts
@@ -5,19 +5,13 @@ export class AddedLinkMinimalIconsLocation1632766796606 implements MigrationInte
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "public"."link" DROP COLUMN "icon"`);
-        await queryRunner.query(`ALTER TABLE "public"."user" ADD "emailActivationCode" character varying`);
-        await queryRunner.query(`ALTER TABLE "public"."user" ADD "forgotPasswordCode" character varying`);
-        await queryRunner.query(`ALTER TABLE "public"."user" ADD "tokenCode" character varying`);
         await queryRunner.query(`ALTER TABLE "public"."link" ADD "iconColorful" character varying`);
         await queryRunner.query(`ALTER TABLE "public"."link" ADD "iconMinimal" character varying`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "public"."link" DROP COLUMN "iconMinimal"`);
-        await queryRunner.query(`ALTER TABLE "public"."link" DROP COLUMN "iconColorful"`);
-        await queryRunner.query(`ALTER TABLE "public"."user" DROP COLUMN "tokenCode"`);
-        await queryRunner.query(`ALTER TABLE "public"."user" DROP COLUMN "forgotPasswordCode"`);
-        await queryRunner.query(`ALTER TABLE "public"."user" DROP COLUMN "emailActivationCode"`);
+        await queryRunner.query(`ALTER TABLE "public"."link" DROP COLUMN "iconColorful"`);;
         await queryRunner.query(`ALTER TABLE "public"."link" ADD "icon" character varying`);
     }
 
