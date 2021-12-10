8f66bc9220c48b82d7c7d16f09b7748e4a5770ff
Rashedul Alam <rashedulalam112112@gmail.com>
2021-12-10 11:33:01 +0600

    Updated logs

--- src/adminbro/resources/activity/logs.options.ts
+++ src/adminbro/resources/activity/logs.options.ts
@@ -4,33 +4,15 @@ import { UserLogs } from '../../../entities'
 
 export const logsOptions: ResourceOptions = {
   listProperties: ['userId', 'ipAddress', 'deviceType', 'countryCode', 'description', 'createdAt'],
-  filterProperties: [
-    'userId',
-    'ipAddress',
-    'deviceType',
-    'osName',
-    'browserName',
-    'browserLanguage',
-    'cityName',
-    'countryCode',
-    'description',
-    'createdAt',
-  ],
-  showProperties: [
-    'userId',
-    'ipAddress',
-    'deviceType',
-    'osName',
-    'browserName',
-    'browserLanguage',
-    'cityName',
-    'countryCode',
-    'description',
-    'createdAt',
-  ],
   actions: {
     show: {
       showInDrawer: true,
+    },
+    edit: {
+      isVisible: false,
+    },
+    new: {
+      isVisible: false,
     },
   },
   navigation: {
