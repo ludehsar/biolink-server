08611ab5283aebfcfdf81d8bfc0a63c05582ea50
Rashedul Alam <rashedulalam112112@gmail.com>
2021-12-15 19:09:31 +0600

    Fixing payment union

--- src/json-types/PaymentRecordUnion.ts
+++ src/json-types/PaymentRecordUnion.ts
@@ -4,4 +4,13 @@ import { PaypalPaymentRecord, StripeInvoiceObject } from '../json-types'
 export const PaymentRecordUnion = createUnionType({
   name: 'PaymentRecord',
   types: () => [StripeInvoiceObject, PaypalPaymentRecord] as const,
+  resolveType: (value) => {
+    if ('object' in value) {
+      return StripeInvoiceObject
+    }
+    if ('payer' in value) {
+      return PaypalPaymentRecord
+    }
+    return undefined
+  },
 })
