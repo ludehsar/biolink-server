62a4f885d0a0d223798b14e2f4db1ce56328b372
Rashedul Alam <rashedulalam112112@gmail.com>
2021-12-17 17:10:06 +0600

    Reverting changes

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -16,7 +16,6 @@ import { AccessAndRefreshToken } from '../object-types/auth/AccessAndRefreshToke
 import { EmailService } from '../services/email.service'
 import { Biolink, BlackList, Code, Plan, User, Username } from '../entities'
 import { stripe } from '../utilities'
-import { NotificationService } from '../services/notification.service'
 
 @Service()
 export class AuthController {
@@ -24,7 +23,6 @@ export class AuthController {
     private readonly authService: AuthService,
     private readonly tokenService: TokenService,
     private readonly emailService: EmailService,
-    private readonly notificationService: NotificationService,
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     @InjectRepository(Username) private readonly usernameRepository: Repository<Username>,
     @InjectRepository(BlackList) private readonly blackListRepository: Repository<BlackList>,
@@ -182,7 +180,7 @@ export class AuthController {
 
     // await this.sendVerificationEmail(context)
 
-    await this.notificationService.createUserLogs(user, context, 'Created new account', true)
+    // await this.notificationService.createUserLogs(user, context, 'Created new account', true)
 
     return {
       access,
