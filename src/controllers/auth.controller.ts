118664d89a05fc6123b071c80aa1429dbd6c6965
Rashedul Alam <rashedulalam112112@gmail.com>
2021-12-17 17:25:51 +0600

    Removed notification creating logs from login or registration, because it is causing infinite requests in heroku

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -16,7 +16,6 @@ import { AccessAndRefreshToken } from '../object-types/auth/AccessAndRefreshToke
 import { EmailService } from '../services/email.service'
 import { Biolink, BlackList, Code, Plan, User, Username } from '../entities'
 import { stripe } from '../utilities'
-import { NotificationService } from '../services/notification.service'
 
 @Service()
 export class AuthController {
@@ -24,7 +23,6 @@ export class AuthController {
     private readonly authService: AuthService,
     private readonly tokenService: TokenService,
     private readonly emailService: EmailService,
-    private readonly notificationService: NotificationService,
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     @InjectRepository(Username) private readonly usernameRepository: Repository<Username>,
     @InjectRepository(BlackList) private readonly blackListRepository: Repository<BlackList>,
@@ -180,9 +178,7 @@ export class AuthController {
 
     context.user = user
 
-    // await this.sendVerificationEmail(context)
-
-    // await this.notificationService.createUserLogs(user, context, 'Created new account', true)
+    await this.sendVerificationEmail(context)
 
     return {
       access,
@@ -197,13 +193,6 @@ export class AuthController {
       loginInput.password
     )
     const { access, refresh } = await this.tokenService.generateAuthTokens(user, context.res)
-
-    await this.notificationService.createUserLogs(
-      user,
-      context,
-      `User logged in on ${moment.now().toLocaleString()}`,
-      true
-    )
 
     return {
       access,
